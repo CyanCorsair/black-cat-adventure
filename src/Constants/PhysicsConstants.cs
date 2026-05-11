e2f5cb497201573b2f466cfb1f32b28a8a9efb27
Silver Salmu <silest@live.com>
2026-05-11 12:05:22 +0200

    Adjust thresholds for basic planet sizes

--- src/Constants/PhysicsConstants.cs
+++ src/Constants/PhysicsConstants.cs
@@ -16,8 +16,8 @@ public static class PhysicsConstants
     public static readonly double MaxStarDensity = 4.0;
     public static readonly double GravitationalConstant = 1.0;
     public static readonly double SpreadVariance = 0.4;
-    public static readonly double MediumPlanetMassBreakpoint = 2.0;
-    public static readonly double LargePlanetMassBreakpoint = 5.0;
+    public static readonly double MediumPlanetMassBreakpoint = 10.0;
+    public static readonly double LargePlanetMassBreakpoint = 40.0;
     public static readonly double MinOrbitalRadius = 100.0;
     public static readonly double MaxOrbitalRadius = 10000.0;
     public static readonly double MinMoonOrbitalRadius = 100.0;
